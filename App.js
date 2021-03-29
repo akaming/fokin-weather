cec580864f71b88ad0636f54787200b3c8e1b45d
myungmin <audals2478@gmail.com>
2021-03-29 17:02:05 +0900

    날씨앱아이콘추가

--- App.js
+++ App.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "3f3e548d93fc93aa74a273efbede8c23";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);  
-  }
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
   componentDidMount() {
-   this.Lo
+    this.getLocation();
   }
   render() {
-    return <Loading/>;
+    const {isLoading, temp, condition} = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>;
   }
 }
 
